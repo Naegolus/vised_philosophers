6807386f3258b6d48de5ed435ab68af616b89429
Helmut <redrocket@gmx.at>
2015-04-11 21:43:14 +0200

    vised - Concurrency-Pattern - Add speedup test

--- speedup.sh
+++ speedup.sh
@@ -4,10 +4,10 @@ TIMEFORMAT=%R
 
 echo 
 echo "Executing single"
-timeSingle=$( { time ./obj/single 1>/dev/null; } 2>&1)
+timeSingle=$( { time ./obj/single $1 1>/dev/null; } 2>&1)
 echo "Time single: "$timeSingle
 echo "Executing multi"
-timeMulti=$( { time ./obj/multi 1>/dev/null; } 2>&1)
+timeMulti=$( { time ./obj/multi $1 1>/dev/null; } 2>&1)
 echo "Time multi: "$timeMulti
 
 speedup=$(echo "scale=2; $timeSingle / $timeMulti" | bc)
