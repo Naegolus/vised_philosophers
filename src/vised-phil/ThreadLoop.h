df803b72abdda8d00536ede4a5db9f62ba4d9417
Johannes Natter <office@dsp-crowd.com>
2017-10-16 19:46:41 +0200

    implement ThreadLoop in header file

--- src/vised-phil/ThreadLoop.h
+++ src/vised-phil/ThreadLoop.h
@@ -1,62 +1,186 @@
 /*
- -----------------------------------------------------------------------------
- -----------------------------------------------------------------------------
- --                                                                         --
- --  This file is part of the Visual Software Editor project                --
- --  https://github.com/Naegolus/vised                                      --
- --                                                                         --
- --  Author(s):                                                             --
- --      - Helmut, redrocket@gmx.at                                         --
- --                                                                         --
- -----------------------------------------------------------------------------
- --                                                                         --
- --  Copyright (C) 2015 Authors                                             --
- --                                                                         --
- --  This program is free software: you can redistribute it and/or modify   --
- --  it under the terms of the GNU General Public License as published by   --
- --  the Free Software Foundation, either version 3 of the License, or      --
- --  (at your option) any later version.                                    --
- --                                                                         --
- --  This program is distributed in the hope that it will be useful,        --
- --  but WITHOUT ANY WARRANTY; without even the implied warranty of         --
- --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           --
- --  GNU General Public License for more details.                           --
- --                                                                         --
- --  You should have received a copy of the GNU General Public License      --
- --  along with this program. If not, see <http://www.gnu.org/licenses/>.   --
- --                                                                         --
- -----------------------------------------------------------------------------
- ----------------------------------------------------------------------------- */
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  Copyright (C) 2017 Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef THREADLOOP_H_
 #define THREADLOOP_H_
 
 #include <cstdint>
 #include <thread>
+#include <chrono>
+#include <ctime>
+#include <iostream>
 #include "Object.h"
 
 class ThreadLoop : public Object
 {
 public:
-	ThreadLoop();
-	virtual ~ThreadLoop();
+	ThreadLoop() : _p_thread(NULL),
+				_timerInterval(20),
+				_threadRunning(false),
+				_threadIsDone(true)
+	{
+	}
 
-	void start();
-	void start(const uint32_t msec);
-	void requestFinish();
-	bool isFinished() const;
-	void join();
-	bool join(const uint32_t timeout_ms);
-	void shutdown();
-	bool shutdown(const uint32_t timeout_ms);
-	void kill();
+	virtual ~ThreadLoop()
+	{
+		if(_p_thread)
+			kill();
+	}
 
+	void start()
+	{
+		if(_p_thread) {
+			std::cerr << "ThreadLoop: Thread isn't shutdown" << std::endl;
+			return;
+		}
+
+		_threadRunning = true;
+		_threadIsDone = false;
+		_p_thread = new std::thread(&ThreadLoop::tick, this);
+	}
+
+	void start(const uint32_t msec)
+	{
+		if(!msec) {
+			std::cerr << "ThreadLoop: Interval is zero" << std::endl;
+			return;
+		}
+
+		if(_p_thread) {
+			std::cerr << "ThreadLoop: Thread isn't shutdown" << std::endl;
+			return;
+		}
+
+		_timerInterval = msec;
+
+		start();
+	}
+
+	void requestFinish()
+	{
+		_threadRunning = false;
+	}
+
+	bool isFinished() const
+	{
+		return _threadIsDone;
+	}
+
+	void join()
+	{
+		if(!_p_thread)
+			return;
+
+		if(std::this_thread::get_id() == _p_thread->get_id()) {
+			std::cerr << "ThreadLoop: Design error. join() called by own thread => Deadlock" << std::endl;
+			std::cerr << "  Must be called by different thread!" << std::endl;
+			return;
+		}
+
+		_p_thread->join();
+		kill();
+	}
+
+	bool join(const uint32_t timeout_ms)
+	{
+		return joinOrShutdown(timeout_ms, false);
+	}
+
+	void shutdown()
+	{
+		requestFinish();
+		join();
+	}
+
+	bool shutdown(const uint32_t timeout_ms)
+	{
+		return joinOrShutdown(timeout_ms, true);
+	}
+
+	void kill()
+	{
+		if(!_p_thread) {
+			delete _p_thread;
+			_p_thread = 0;
+			_threadRunning = false;
+			_threadIsDone = true;
+		}
+	}
+
+	/* signals */
 	signal0<> ticked;
 
 private:
-	void tick();
-	bool joinOrShutdown(const uint32_t timeout_ms, bool shutdown);
-	bool killedByTimeout(uint32_t timeout_ms);
+	void tick()
+	{
+		std::chrono::milliseconds interval(_timerInterval);
+
+		while(_threadRunning) {
+			std::this_thread::sleep_for(interval);
+			ticked();
+		}
+
+		_threadIsDone = true;
+	}
+
+	bool joinOrShutdown(const uint32_t timeout_ms, bool shutdown)
+	{
+		if(!_p_thread)
+			return true; /* already shutdown */
+
+		if(std::this_thread::get_id() == _p_thread->get_id()) {
+			std::cerr << "ThreadLoop: Design error. join() or shutdown() called by own thread => Deadlock" << std::endl;
+			std::cerr << "  Must be called by different thread!" << std::endl;
+			return false;
+		}
+
+		if(shutdown)
+			requestFinish();
+
+		if(killedByTimeout(timeout_ms))
+			return false;
+
+		_p_thread->join();
+		kill();
+
+		return true;
+	}
+
+	bool killedByTimeout(uint32_t timeout_ms)
+	{
+		clock_t t;
+
+		t = clock();
+		while(!isFinished()) {
+			if(((float)(clock() - t) * (TICK_TO_SEC * SEC_TO_MS)) > timeout_ms) {
+				std::cerr << "ThreadLoop: Timeout => Have to kill() thread" << std::endl;
+				kill();
+				return true;
+			}
+		}
+
+		return false;
+	}
 
 	std::thread *_p_thread;
 	uint32_t _timerInterval;
